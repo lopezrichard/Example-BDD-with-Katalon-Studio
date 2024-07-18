b7e0d1b3638bcc32f95501962ab58be8e17d0fb1
lopezrichard <84924555+lopezrichard@users.noreply.github.com>
2024-07-18 15:53:11 -0400

    example

--- Test Suites/Cucumber Test-Suite.ts
+++ Test Suites/Cucumber Test-Suite.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Cucumber Test Suite</name>
+   <name>Cucumber Test-Suite</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
